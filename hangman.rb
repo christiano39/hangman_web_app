6f1e0d045382efd45fc2259e099a93bd9f1ac603
christiano39 <carneson39@gmail.com>
2019-12-22 23:27:58 -0700

    Basic functional hangman game

---
+++ hangman.rb
@@ -0,0 +1,99 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+def select_random_word
+    dictionary = File.readlines("dictionary.txt")
+    random_index = get_new_random_number
+    begin
+        dictionary[random_index].to_s.gsub!(/\r/,"").gsub!(/\n/,"").trim
+    rescue
+        dictionary[random_index].to_s
+    end
+end
+
+def select_good_word
+    word = select_random_word
+    unless word.length > 5 && word.length <= 12
+        word = select_random_word
+    end
+    word.downcase
+end
+
+def get_new_random_number
+    filename = "dictionary.txt"
+    line_count = `wc -l "#{filename}"`.strip.split(' ')[0].to_i
+    rand(line_count)
+end
+
+def is_match?(word, char)
+    if word.include? char
+        true
+    else
+        false
+    end
+end
+
+def secret_word_to_underscores(secret_word)
+    length = secret_word.length - 1
+    new_word = "".rjust(length * 2, "_ ")
+    new_word = new_word.split(" ")
+end
+
+def get_indices(word, char)
+    word = word.split("")
+    indices = word.each_index.select{|i| word[i] == char}
+end
+
+def reset_game
+    $secret_word = select_good_word
+    $guessed_letters = []
+    $number_of_guesses_remaining = 8
+    $guessed_word = secret_word_to_underscores($secret_word)
+    $message = ""
+end
+
+def enable_cheat_mode
+    $message += "Cheat mode enabled: Secret word is #{$secret_word}."
+end
+
+$secret_word = select_good_word
+$guessed_letters = []
+$number_of_guesses_remaining = 8
+$guessed_word = secret_word_to_underscores($secret_word)
+$message = ""
+
+get '/' do
+    user_guess = params['user_guess']
+    game_over = false
+    if !user_guess.nil?
+        if is_match?($secret_word, user_guess)
+            indices = get_indices($secret_word, user_guess)
+            indices.each do |i|
+                $guessed_word[i] = user_guess
+            end
+        else
+            unless $guessed_letters.include? user_guess
+                $guessed_letters.push(user_guess)
+                $number_of_guesses_remaining -= 1
+            end
+        end
+    end
+    if $number_of_guesses_remaining == 0
+        game_over = true
+        $message = "Darn! Out of guesses. The secret word was #{$secret_word}."
+    elsif !$guessed_word.include? '_'
+        game_over = true
+        $message = "Nice job! You guessed the word #{$secret_word}."
+    end
+    if params['reset']
+        reset_game
+        game_over = false
+    end
+    if params['cheat'] == 'true'
+        enable_cheat_mode
+    end
+    erb :index, :locals => {:secret_word => $secret_word, 
+        :guessed_word => $guessed_word, :guessed_letters => $guessed_letters,
+        :number_of_guesses_remaining => $number_of_guesses_remaining,
+        :message => $message, :game_over => game_over}
+end
